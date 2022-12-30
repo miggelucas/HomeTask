82a45332bfaaf18e2063e6ed83a34bceacc2334c
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2022-12-30 16:22:09 -0300

    Inciando projeto no XCode

---
+++ HomeTask/HomeTask/HomeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTaskApp.swift
+//  HomeTask
+//
+//  Created by Lucas Migge de Barros on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
